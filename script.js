d94cbe59011d8610c853b86483036fd85cad55e7
Farukh <abdukhadyrovf@gmail.com>
2026-02-23 16:30:52 +0600

    update jvscript

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const blueHashColor = "#0000FF";
 
 recolorFirstCardButton.addEventListener('click', () => {
   firstProductCard.style.backgroundColor = blueHashColor;
-})
+});
 
 // покраска всех карточек 
 
@@ -17,28 +17,28 @@ const recolorAllCardButton = document.getElementById('recolor-all-card-button');
 
 recolorAllCardButton.addEventListener('click', () => {
   allProductCard.forEach(
-    card =>card.style .backgroundColor = greenHashColor
-  )
-})
+    card => card.style .backgroundColor = greenHashColor
+  );
+});
 
 // переход на страницу гугл 
 
 const openGoogleButton = document.getElementById('open-google-button');
 openGoogleButton.addEventListener('click', openGoogle);
 
-function openGoogle(){
-  const answer = confirm ('Вы действительно хотите перейти на сайт Google?')
+function openGoogle() {
+  const answer = confirm ('Вы действительно хотите перейти на сайт Google?');
   
-  if(answer === true) {
-    window.open('https://google.com')
+  if (answer === true) {
+    window.open('https://google.com');
   } else {
-    console.log('Пользователь отменил переход на Google')
+    console.log('Пользователь отменил переход на Google');
   }
-}
+};
 // Изменить цвет кнопки
 
 const changeColorButton = document. getElementById('change-color-button');
 changeColorButton.addEventListener('click', () => {
   const yellowHashColor = '#FFFF00';
-  changeColorButton.style.backgroundColor=yellowHashColor;
-})
+  changeColorButton.classList.toggle ('active')
+}); 
